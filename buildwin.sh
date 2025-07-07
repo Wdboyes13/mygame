05dcd17d54b12b07a239cd6cdee1b5f9a839bb62
Wdboyes13 <willdev2025@outlook.com>
2025-07-06 19:13:46 -0700

    Sun  6 Jul 2025 19:13:46 PDT

--- buildwin.sh
+++ buildwin.sh
@@ -1,6 +1,7 @@
 x86_64-w64-mingw32-gcc \
   -I/opt/will/include \
-  src/views/MainHelpers/UpdatePos.c src/StartMusic.c src/Setup.c src/views/MainView.c src/Game.c src/views/DeadView.c \
+  src/views/MainHelpers/UpdatePos.c src/StartMusic.c src/Setup.c \
+  src/views/MainView.c src/Game.c src/views/DeadView.c \
   -static-libgcc -static-libstdc++ \
   -L../WinSDL/ \
   -lmingw32 -lSDL2main -lSDL2 -lSDL2_mixer -lSDL2_image -lgdi32 -luser32 \
