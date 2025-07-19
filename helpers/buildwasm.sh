a7287635f250fe7c6db5606531d2d81ef94c35df
Wdboyes13 <willdev2025@outlook.com>
2025-07-19 08:45:45 -0700

    ee

--- helpers/buildwasm.sh
+++ helpers/buildwasm.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 emcc --preload-file res src/Game.c src/Setup.c src/StartMusic.c \
     src/views/DeadView.c src/views/MainView.c src/views/MainHelpers/UpdatePos.c \
+    src/views/MenuView.c src/views/WinView.c src/views/CredsView.c \
+    src/ScoreRenderer/Init.c src/ScoreRenderer/RenderNum.c \
     -o index.html -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_MIXER=2 -s WASM=1 \
     -s EXIT_RUNTIME=1  -s FORCE_FILESYSTEM=1 -s ALLOW_MEMORY_GROWTH=1 \
     -s INITIAL_MEMORY=134217728 -s EXPORTED_RUNTIME_METHODS='["requestFullscreen"]' \
